c71620f9181bd489715398793bce2eb11c184305
mu2so4 <b4.orangutang@gmail.com>
2023-03-13 13:35:36 +0700

    Query 1: Add primary query

---
+++ 1/primary.sql
@@ -0,0 +1,28 @@
+SELECT
+	"Employees"."Personnel_number",
+	"Last_name",
+	"First_name",
+	"Patronymic",
+	"Birthday",
+	"Genders"."Gender_name" AS "Sex",
+	"Hire_date",
+	"Salary",
+	"Professions"."Profession_name" AS "Profession",
+	"Children_count",
+	"Departments"."Department_name" AS "Department"
+FROM "Employees"
+	INNER JOIN "Genders"
+		ON "Employees"."Sex" = "Genders"."Gender_id"
+	INNER JOIN "Professions"
+		ON "Employees"."Profession_id" = "Professions"."Profession_id"
+	INNER JOIN "Departments"
+		ON "Employees"."Department_id" = "Departments"."Department_id"
+ORDER BY
+	"Last_name",
+	"First_name",
+	"Patronymic";
+
+
+SELECT
+	COUNT(*)
+FROM "Employees";
