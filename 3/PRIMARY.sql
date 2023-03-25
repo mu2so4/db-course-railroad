d0ceb4ddaaa94d67ea23a2c84df2a30ac0450b6c
mu2so4 <b4.orangutang@gmail.com>
2023-03-25 16:04:01 +0700

    Query 3: Поправил первичный запрос

--- 3/PRIMARY.sql
+++ 3/PRIMARY.sql
@@ -1,21 +1,18 @@
 SELECT
-	"Personnel_number" AS "Табельный номер",
-	"Last_name" AS "Фамилия",
-	"First_name" AS "Имя",
-	"Patronymic" AS "Отчество",
-	"Birthday" AS "Дата рождения",
-	"Genders"."Gender_name" AS "Пол",
-	"Hire_date" AS "Дата приёма на работу",
-	"Salary" AS "Зарплата, р",
-	"Professions"."Profession_name" AS "Профессия",
-	"Children_count" AS "Количество детей",
-	"Departments"."Department_name" AS "Отдел"
-FROM "Employees"
-	INNER JOIN "Professions"
-		ON "Employees"."Profession_id" = "Professions"."Profession_id"
-	INNER JOIN "Departments"
-		ON "Employees"."Department_id" = "Departments"."Department_id"
-	INNER JOIN "Genders"
-		ON "Employees"."Sex" = "Genders"."Gender_id"
+	"Id",
+	"Examination_date",
+	"Work_permit",
+	"Personnel_number",
+	"Last_name",
+	"First_name",
+	"Patronymic",
+	"Brigade_id",
+	"Brigade_name"
+FROM "Workers"
+	INNER JOIN "Brigades" USING("Brigade_id")
+	INNER JOIN "Medical_examinations"
+		ON "Workers"."Personnel_number" = "Medical_examinations"."Employee"
 WHERE
-	"Professions"."Profession_name" = 'Машинист';
+	"Profession_id" = 1
+ORDER BY
+	"Examination_date";
