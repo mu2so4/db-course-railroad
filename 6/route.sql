99e08980ce27c008b7e982dbb2e381f8d77ec139
mu2so4 <b4.orangutang@gmail.com>
2023-04-03 23:19:11 +0700

    Query 6: Добавил просмотр по отдельному маршруту

---
+++ 6/route.sql
@@ -0,0 +1,11 @@
+SELECT
+	"Trip_id",
+	"Departure_station",
+	"Arrival_station",
+	"Departure_time",
+	"Arrival_time"
+FROM "Timetable"
+WHERE
+	NOT "Is_cancelled" AND
+	'2023-01-17 16:20:38' BETWEEN "Departure_time" AND "Arrival_time" AND -- param
+	"Route_id" = 348;
