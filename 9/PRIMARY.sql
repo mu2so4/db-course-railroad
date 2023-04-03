bf80be58b842550e4ef95f9a30459cba14155a8a
mu2so4 <b4.orangutang@gmail.com>
2023-04-03 22:08:08 +0700

    Query 9: Добавил первичный запрос

--- 9/PRIMARY.sql
+++ 9/PRIMARY.sql
@@ -1,31 +1,17 @@
+WITH "Counts" AS (
+	SELECT
+		"Route_id",
+		COUNT(*) AS "Sold_count"
+	FROM "Trips"
+		INNER JOIN "Tickets" USING("Trip_id")
+	GROUP BY
+		"Route_id"
+)
+
 SELECT
-	"Ticket_id",
-	"Trips"."Trip_id",
-	"Last_name",
-	"First_name",
-	"Patronymic",
-	"Genders"."Gender_name" AS "Sex",
-	"Passport_number",
-	"Cost",
-	"Departure_stations"."Full_name" AS "Departure_station",
-	"Trips"."Start_date" + "Departure_stops"."Departure_time" AS "Departure_time",
-	"Arrival_stations"."Full_name" AS "Arrival_station",
-	"Trips"."Start_date" + "Arrival_stops"."Arrival_time" AS "Arrival_time",
-	"Place",
-	"Purchase_time",
-	"Return_time"
-FROM "Tickets"
-	INNER JOIN "Trips"
-		ON "Tickets"."Trip_id" = "Trips"."Trip_id"
-	INNER JOIN "Stations" AS "Departure_stations"
-		ON "Tickets"."Departure_station" = "Departure_stations"."Station_id"
-	INNER JOIN "Station_stops" AS "Departure_stops"
-		ON "Departure_stations"."Station_id" = "Departure_stops"."Station_id" AND
-			"Trips"."Route_id" = "Departure_stops"."Route_id"
-	INNER JOIN "Stations" AS "Arrival_stations"
-		ON "Tickets"."Arrival_station" = "Arrival_stations"."Station_id"
-	INNER JOIN "Station_stops" AS "Arrival_stops"
-		ON "Arrival_stations"."Station_id" = "Arrival_stops"."Station_id" AND
-			"Trips"."Route_id" = "Arrival_stops"."Route_id"
-	INNER JOIN "Genders"
-		ON "Tickets"."Sex_id" = "Genders"."Gender_id"
+	"Route_id",
+	COALESCE("Counts"."Sold_count", 0) AS "Sold_count"
+FROM "Routes"
+	LEFT OUTER JOIN "Counts" USING("Route_id")
+ORDER BY
+	"Route_id"
