6afbd6ff9c7294bacce49c19da79b27d7ea2e084
mu2so4 <b4.orangutang@gmail.com>
2023-04-03 19:28:39 +0700

    Query 4: Привёл к новой схеме БД

--- 4/PRIMARY.sql
+++ 4/PRIMARY.sql
@@ -5,13 +5,11 @@ SELECT
 	"Home_station",
 	"Stations"."Full_name" AS "Station_name",
 	"Locomotive_brigade",
-	"Locomotive_brigades"."Brigade_name" as "Locomotive_brigade_name",
-	"Repair_brigade",
-	"Repair_brigades"."Brigade_name" as "Repair_brigade_name"
+	"Repair_brigade"
 FROM "Locomotives"
 	INNER JOIN "Stations"
 		ON "Locomotives"."Home_station" = "Stations"."Station_id"
-	INNER JOIN "Locomotive_brigades"
-		ON "Locomotives"."Locomotive_brigade" = "Locomotive_brigades"."Brigade_id"
-	INNER JOIN "Repair_brigades"
-		ON "Locomotives"."Repair_brigade" = "Repair_brigades"."Brigade_id";
+	LEFT OUTER JOIN "Locomotive_workers"
+		ON "Locomotives"."Locomotive_number" = "Locomotive_workers"."Locomotive"
+ORDER BY
+	"Build_date"
