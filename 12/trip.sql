801f92e6ad6feb4ec07d5ff7b72127b08ded2aa5
mu2so4 <b4.orangutang@gmail.com>
2023-04-04 09:13:30 +0700

    Query 12: Упростил запрос по конкретному рейсу

--- 12/trip.sql
+++ 12/trip.sql
@@ -1,30 +1,14 @@
-WITH "Sold_counts" AS (
-	SELECT
-		"Trip_id",
-		COUNT(*) AS "Sold_count"
-	FROM "Trips"
-		INNER JOIN "Tickets" USING("Trip_id")
-		INNER JOIN "Station_stops" "Departure_stops"
-			ON "Trips"."Route_id" = "Departure_stops"."Route_id" AND
-			"Tickets"."Departure_station" = "Departure_stops"."Station_id"
-		INNER JOIN "Station_stops" "Arrival_stops"
-			ON "Trips"."Route_id" = "Arrival_stops"."Route_id" AND
-			"Tickets"."Arrival_station" = "Arrival_stops"."Station_id"
-		INNER JOIN "Station_stops" "Our_stops"
-			ON "Trips"."Route_id" = "Our_stops"."Route_id" AND
-			current_station_id() = "Our_stops"."Station_id"
-	WHERE
-		"Our_stops"."Departure_time" BETWEEN "Departure_stops"."Departure_time" AND
-			"Arrival_stops"."Arrival_time" AND
-		"Return_time" IS NULL
-	GROUP BY
-		"Trip_id"
-)
 SELECT
 	"Trips"."Trip_id",
 	"Place_count",
-	"Place_count" - COALESCE("Sold_count", 0) AS "Remaining_count"
+	"Place_count" - COUNT("Tickets"."Ticket_id") AS "Remaining_count"
 FROM "Trips"
-	LEFT OUTER JOIN "Sold_counts" USING("Trip_id")
+	LEFT OUTER JOIN "Tickets"
+		ON "Trips"."Trip_id" = "Tickets"."Trip_id" AND
+		"Return_time" IS NULL
 WHERE
-	"Trip_id" = 4 -- param
+	"Trips"."Trip_id" = 4 -- param
+GROUP BY
+	"Trips"."Trip_id"
+ORDER BY
+	"Trips"."Trip_id"
