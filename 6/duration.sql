c45967e296ba6dbd65c02face0390ad75e467382
mu2so4 <b4.orangutang@gmail.com>
2023-04-04 15:38:24 +0700

    Query 6: Добавил фильтрацию по длительности рейса

--- 6/duration.sql
+++ 6/duration.sql
@@ -2,10 +2,11 @@ SELECT
 	"Trip_id",
 	"Departure_station",
 	"Arrival_station",
-	"Departure_time",
-	"Arrival_time"
+	"Arrival_time" - "Departure_time" AS "Duration"
 FROM "Timetable"
 WHERE
 	NOT "Is_cancelled" AND
 	'2023-01-17 16:20:38' BETWEEN "Departure_time" AND "Arrival_time" AND -- param
-	"Route_id" = 348; -- param
+	"Arrival_time" - "Departure_time" BETWEEN '48:00' AND '72:00' -- params
+ORDER BY
+	"Trip_id"
