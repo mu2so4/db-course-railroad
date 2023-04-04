2147e3eee5dd2c76a3f0cff402d2f6bf4110434a
mu2so4 <b4.orangutang@gmail.com>
2023-04-04 09:23:42 +0700

    Query 4: Упростил запрос получения количества совершённых рейсов

--- 4/trip_count.sql
+++ 4/trip_count.sql
@@ -1,17 +1,13 @@
-WITH "Pure_counts" AS (
-	SELECT
-		"Locomotive" AS "Locomotive_number",
-		COUNT(*) AS "Trip_count"
-	FROM "Trips"
-	WHERE
-		NOT "Is_cancelled"
-	GROUP BY
-		"Locomotive"
-)
 SELECT
 	"Locomotive_number",
-	COALESCE("Pure_counts"."Trip_count", 0) AS "Trip_count"
+	"Model",
+	"Home_station",
+	COUNT("Trip_id") AS "Trip_count"
 FROM "Locomotives"
-	LEFT OUTER JOIN "Pure_counts" USING("Locomotive_number")
+	LEFT OUTER JOIN "Trips"
+		ON "Locomotives"."Locomotive_number" = "Trips"."Locomotive" AND
+		NOT "Trips"."Is_cancelled"
+GROUP BY
+	"Locomotive_number"
 ORDER BY
 	"Locomotive_number"
