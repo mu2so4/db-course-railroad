06d1c32337a7597fdc927af46556eae980a1718a
mu2so4 <b4.orangutang@gmail.com>
2023-04-04 09:18:23 +0700

    Query 9: Упростил первичный запрос

--- 9/PRIMARY.sql
+++ 9/PRIMARY.sql
@@ -1,17 +1,10 @@
-WITH "Counts" AS (
-	SELECT
-		"Route_id",
-		COUNT(*) AS "Sold_count"
-	FROM "Trips"
-		INNER JOIN "Tickets" USING("Trip_id")
-	GROUP BY
-		"Route_id"
-)
-
 SELECT
 	"Route_id",
-	COALESCE("Counts"."Sold_count", 0) AS "Sold_count"
+	COUNT("Tickets"."Ticket_id") AS "Sold_count"
 FROM "Routes"
-	LEFT OUTER JOIN "Counts" USING("Route_id")
+	LEFT OUTER JOIN "Trips" USING("Route_id")
+	LEFT OUTER JOIN "Tickets" USING("Trip_id")
+GROUP BY
+	"Route_id"
 ORDER BY
 	"Route_id"
